eaaa46e375bc0a33ff7477b1d946101b78f577dc
truongthuanhung <hung.truongthuan@hcmut.edu.vn>
2024-10-21 19:06:46 +0700

    add header, logout service

--- src/services/http.ts
+++ src/services/http.ts
@@ -27,7 +27,7 @@ http.interceptors.response.use(
   },
   async (error) => {
     const originalConfig = error.config;
-    if (error.response && error.response.status == 401 && error.response.data?.message === 'jwt expired') {
+    if (error.response && error.response.status == 401 && error.response.data?.message === 'jwt access_token expired') {
       try {
         const response = await refreshToken({ refresh_token: localStorage.getItem('refresh_token') as string });
         const { access_token, refresh_token } = response.data.result;
@@ -36,13 +36,18 @@ http.interceptors.response.use(
         localStorage.setItem('refresh_token', refresh_token);
         originalConfig.headers['Authorization'] = `Bearer ${access_token}`;
 
+        if (originalConfig.data) {
+          const parsedData = JSON.parse(originalConfig.data);
+          if (parsedData.refresh_token) {
+            parsedData.refresh_token = refresh_token;
+            originalConfig.data = JSON.stringify(parsedData);
+          }
+        }
         return http(originalConfig);
       } catch (err: any) {
-        console.log('run catch');
-        if (err.response && err.response.status === 401 && err.response.data?.message === 'jwt expired') {
+        if (err.response && err.response.status === 401 && err.response.data?.message === 'jwt refresh_token expired') {
           localStorage.removeItem('access_token');
           localStorage.removeItem('refresh_token');
-          localStorage.removeItem('user');
           window.location.href = '/login';
         }
         return Promise.reject(err);
